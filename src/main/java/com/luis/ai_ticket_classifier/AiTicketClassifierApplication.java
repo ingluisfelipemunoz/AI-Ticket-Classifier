05a927d5c24a2e67071054424f91858ab58f970d
Luis F. Munoz <ingluisfelipemunoz@gmail.com>
2026-06-05 20:33:50 -0400

    enabling configuration properties for openai

--- src/main/java/com/luis/ai_ticket_classifier/AiTicketClassifierApplication.java
+++ src/main/java/com/luis/ai_ticket_classifier/AiTicketClassifierApplication.java
@@ -1,9 +1,12 @@
 package com.luis.ai_ticket_classifier;
 
+import com.luis.ai_ticket_classifier.ai.OpenAIProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OpenAIProperties.class)
 public class AiTicketClassifierApplication {
 
 	public static void main(String[] args) {
