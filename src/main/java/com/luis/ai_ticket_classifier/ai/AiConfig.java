6cb59afec156126bc07f146bfb3be07f7b6c8423
Luis F. Munoz <ingluisfelipemunoz@gmail.com>
2026-06-05 21:25:52 -0400

    Injecting apikey directly form the environment

--- src/main/java/com/luis/ai_ticket_classifier/ai/AiConfig.java
+++ src/main/java/com/luis/ai_ticket_classifier/ai/AiConfig.java
@@ -1,5 +1,6 @@
 package com.luis.ai_ticket_classifier.ai;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,10 +9,12 @@ import org.springframework.web.client.RestClient;
 public class AiConfig {
 
     @Bean
-    RestClient openAiRestClient(OpenAIProperties properties){
+    RestClient openAiRestClient(
+            OpenAIProperties properties,
+            @Value("${OPENAI_API_KEY}") String apiKey) {
         return RestClient.builder()
                 .baseUrl(properties.baseUrl())
-                .defaultHeader("Authorization", "Bearer " + properties.apiKey())
+                .defaultHeader("Authorization", "Bearer " + apiKey)
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
--- src/main/java/com/luis/ai_ticket_classifier/ai/OpenAIProperties.java
+++ src/main/java/com/luis/ai_ticket_classifier/ai/OpenAIProperties.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "ai.openai")
 public record OpenAIProperties(
-        String apiKey,
         String baseUrl,
         String model
 ) {
